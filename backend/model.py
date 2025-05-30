82c235cd4b012eb7c43cd195c2b6dfcd5b51b8bb
Sotto <121021893+L-Sotto@users.noreply.github.com>
2025-05-30 16:53:30 +0100

    uiui

--- backend/model.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self,user_id,name,password):
-        self.user_id=user_id
-        self.name = name
-        self.password = password
-
-    def to_json(self):
-        return {'user_id': self.user_id,'name': self.name,'password' : self.password}
